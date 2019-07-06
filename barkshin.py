564cf278ee3907c43f1e71c38ff26c7ce33f4287
Joe Heck <heckj@mac.com>
2019-07-06 16:59:41 -0700

    various errors

--- barkshin.py
+++ barkshin.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask import render_template
+from flask import jsonify
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+@app.route('/json')
+def make_json():
+    return jsonify({"name": "fred", "count": 5, "valid": True})
+
+@app.route('/badRequest')
+def make_bad_request():
+    return 'bad request!', 400
+
+@app.route('/generalError')
+def make_general_error():
+    return 'error!', 500
+
 @app.route('/')
-@app.route('/<searchword>')
 def frontpage(searchword=None):
 
     results = []
