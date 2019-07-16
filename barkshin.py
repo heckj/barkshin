a4f1ed7c0eb61354846272a0a0e568911dccf749
Joe Heck <heckj@mac.com>
2019-07-15 18:32:31 -0700

    pylint excuses

--- barkshin.py
+++ barkshin.py
@@ -5,13 +5,13 @@ from flask_jwt import JWT, jwt_required, current_identity
 from werkzeug.security import safe_str_cmp
 
 class User(object):
-    def __init__(self, id, username, password):
-        self.id = id
+    def __init__(self, userid, username, password):
+        self.userid = userid
         self.username = username
         self.password = password
 
     def __str__(self):
-        return "User(id='%s')" % self.id
+        return "User(id='%s')" % self.userid
 
 USERS = [
     User(1, 'user1', 'abcxyz'),
@@ -19,7 +19,7 @@ USERS = [
 ]
 
 USERNAME_TABLE = {u.username: u for u in USERS}
-USERID_TABLE = {u.id: u for u in USERS}
+USERID_TABLE = {u.userid: u for u in USERS}
 
 # endpoint defaults to /auth
 def authenticate(username, password):
@@ -31,13 +31,13 @@ def identity(payload):
     user_id = payload['identity']
     return USERID_TABLE.get(user_id, None)
 
-app = Flask(__name__)
+app = Flask(__name__) # pylint: disable=invalid-name
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = 'notAtAllSecretBarkshin'
 app.config['JWT_AUTH_USERNAME_KEY'] = 'username'
 app.config['JWT_AUTH_PASSWORD_KEY'] = 'password'
 
-jwt = JWT(app, authenticate, identity)
+jwt = JWT(app, authenticate, identity) # pylint: disable=invalid-name
 
 @app.route('/json')
 def make_json():
