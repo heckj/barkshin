bb76b69cacfb4100a37e41d2e1c6268b1aa5a6bb
Joe Heck <heckj@mac.com>
2019-07-15 17:55:50 -0700

    enabling basic JWT

--- barkshin.py
+++ barkshin.py
@@ -1,9 +1,41 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+
+class User(object):
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+users = [
+    User(1, 'user1', 'abcxyz'),
+    User(2, 'user2', 'abcxyz'),
+]
+
+username_table = {u.username: u for u in users}
+userid_table = {u.id: u for u in users}
+
+# endpoint defaults to /auth
+def authenticate(username, password):
+    user = username_table.get(username, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'notAtAllSecretBarkshin'
+
+jwt = JWT(app, authenticate, identity)
 
 @app.route('/json')
 def make_json():
@@ -16,6 +48,11 @@ def make_bad_request():
 @app.route('/generalError')
 def make_general_error():
     return 'error!', 500
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 @app.route('/')
 def frontpage(searchword=None):
